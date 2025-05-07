2719174b6aab63d98ce42e3a25ac8ce6041ad938
MRSM <mrsm@MacBook-Pro-Moh.local>
2025-05-08 05:31:18 +0700

    Without Login Page

--- app/routes/root/sign-in.tsx
+++ app/routes/root/sign-in.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
 import {Link, redirect} from "react-router";
 import {ButtonComponent} from "@syncfusion/ej2-react-buttons";
 import {loginWithGoogle} from "~/appwrite/auth";
 import {account} from "~/appwrite/client";
 
-export async function clientLoader(){
+export async function clientLoader() {
     try {
         const user = await account.get();
-        if(user.$id) return redirect('/dashboard');
+
+        if(user.$id) return redirect('/');
     } catch (e) {
-        console.log('Error fetching user', e);
+        console.log('Error fetching user', e)
     }
 }
 
@@ -20,23 +20,33 @@ const SignIn = () => {
                 <div className="sign-in-card">
                     <header className="header">
                         <Link to="/">
-                            <img src="/assets/icons/logo.svg" alt="logo" className="size-[30px]"/>
+                            <img
+                                src="/assets/icons/logo.svg"
+                                alt="logo"
+                                className="size-[30px]"
+                            />
                         </Link>
                         <h1 className="p-28-bold text-dark-100">Tourvisto</h1>
                     </header>
 
                     <article>
                         <h2 className="p-28-semibold text-dark-100 text-center">Start Your Travel Journey</h2>
-                        <p className="p-18-regular text-center text-gray-100 !leading-7">Sign in with Google to manage destinations, itineraries, and user activity with ease. </p>
+
+                        <p className="p-18-regular text-center text-gray-100 !leading-7">Sign in with Google to manage destinations, itineraries, and user activity with ease.</p>
                     </article>
 
                     <ButtonComponent
-                            type="button"
-                            iconCss="e-search-icon"
-                            className="button-class !h-11 !w-full"
-                            onClick={loginWithGoogle}>
-                        <img src="/assets/icons/google.svg" className="size-5" alt="Google"/>
-                        <span className="p-18-semibold text-white">Sign it with Google</span>
+                        type="button"
+                        iconCss="e-search-icon"
+                        className="button-class !h-11 !w-full"
+                        onClick={loginWithGoogle}
+                    >
+                        <img
+                            src="/assets/icons/google.svg"
+                            className="size-5"
+                            alt="google"
+                        />
+                        <span className="p-18-semibold text-white">Sign in with Google</span>
                     </ButtonComponent>
                 </div>
             </section>
