a5eb68969b6bcaeb9462df43a5cbae695e3fbc66
MRSM <mrsm@MacBook-Pro-Moh.local>
2025-05-07 20:11:20 +0700

    Fixing the true code

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -9,12 +9,12 @@ export async function clientLoader(){
     try {
         const user = await account.get();
 
-        // if(!user.$id) return redirect('/sign-in');
+        if(!user.$id) return redirect('/sign-in');
 
         const existingUser = await getExistingUser(user.$id);
-        // if(existingUser?.status === 'user') {
-        //     return redirect('/');
-        // }
+        if(existingUser?.status === 'user') {
+            return redirect('/');
+        }
 
         return existingUser?.$id ? existingUser : await storeUserData();
 
